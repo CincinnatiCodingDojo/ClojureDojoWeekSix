9ab99d230d1011a2069a7cf42984fc79c833e45a
Kyle Hodgson <kyle.hodgson@gmail.com>
2013-08-19 11:46:53 -0400

    first test

--- app/test/app/core_test.clj
+++ app/test/app/core_test.clj
@@ -2,6 +2,5 @@
   (:require [clojure.test :refer :all]
             [app.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest can-get-cell-contents
+	(is (= ( get-cell 2 2) 1)))
