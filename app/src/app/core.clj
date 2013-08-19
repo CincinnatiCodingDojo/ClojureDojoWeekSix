8ca79a6ac64a91e33762b0e58fbfcd98ccc2d5d2
Brett Timperman <user@Bretts-MacBook-Air.local>
2013-08-19 12:55:56 -0400

    the game of life 8/19

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -4,3 +4,27 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn get-cell [board x y]
+  (if (or (< x 0) (< y 0)) 0
+    (if (or (>= x (count board) )
+          (>= y (count (board x)))) 0 (nth (board x) y))))
+
+(defn get-neighbors [board x y]
+  [(get-cell board (dec x) (dec y))
+   (get-cell board      x  (dec y))
+   (get-cell board (inc x) (dec y))
+   (get-cell board (dec x)      y )
+   (get-cell board (inc x)      y )
+   (get-cell board (dec x) (inc y))
+   (get-cell board      x  (inc y))
+   (get-cell board (inc x) (inc y))]
+)
+
+(defn sum-neighbors [board x y]
+  (reduce + (get-neighbors board x y))
+)
+
+(defn dies-if-under-populated [board x y]
+  (< (sum-neighbors board x y ) 2)
+  )
--- app/test/app/core_test.clj
+++ app/test/app/core_test.clj
@@ -7,5 +7,33 @@
 	 [1 0 1]
 	 [1 1 1]])
 
+(def alive-board
+   [[0 1 0]
+    [1 1 1]
+    [1 1 1]])
+
+(def under-populated-board
+  [[1 0 1]
+   [0 0 0]
+   [1 1 1]])
+
 (deftest can-get-cell-contents
-	(is (= ( get-cell board 2 2) 1)))
+	(is (= ( get-cell board 2 2) 1))
+	(is (= ( get-cell board 2 0) 1))
+	(is (= ( get-cell board 0 2) 0))
+	(is (= ( get-cell board 1 1) 0)))
+
+(deftest can-sum-neighbors
+  (is (= (sum-neighbors board 1 1) 6))
+  (is (= (sum-neighbors alive-board 1 1) 6))
+  (is (= (sum-neighbors alive-board 0 0) 3))
+)
+
+(deftest can-get-neighbors
+  (is (= (get-neighbors board 0 0) [0 0 0 0 1 0 1 0]))
+  (is (= (get-neighbors board 2 2) [0 1 0 1 0 0 0 0]))
+)
+
+(deftest die-if-underpopulated
+  (is (dies-if-under-populated under-populated-board 0 0))
+)
