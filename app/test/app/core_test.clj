9cb7349e6f837429d534e0e40787d97234f52d7d
Kyle Hodgson <kyle.hodgson@gmail.com>
2013-08-19 11:48:43 -0400

    more work on first test

--- app/test/app/core_test.clj
+++ app/test/app/core_test.clj
@@ -2,5 +2,10 @@
   (:require [clojure.test :refer :all]
             [app.core :refer :all]))
 
+(def board
+	[[0 1 0]
+	 [1 0 1]
+	 [1 1 1]])
+
 (deftest can-get-cell-contents
-	(is (= ( get-cell 2 2) 1)))
+	(is (= ( get-cell board 2 2) 1)))
